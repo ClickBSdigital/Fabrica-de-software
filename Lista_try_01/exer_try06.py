32e6f3d84d7f5d47630a3cd88400d942208f6f49
Eliandro Silva <clickbsdigital@gmail.com>
2024-09-03 19:19:09 -0400

    Lista Try 01 Python - F32

---
+++ Lista_try_01/exer_try06.py
@@ -0,0 +1,17 @@
+# 30. Elabore um programa que faça leitura de vários números inteiros, 
+# até que se digite um número
+# negativo. O programa tem que retornar o maior e o menor número lido.
+# #=================================
+num = int(input("DIGITE UM NUMERO; "))
+maior = num
+menor = num
+while num > 0:
+    if num > maior:
+        mairo = num
+    if num < menor:
+        menor = num
+    num = int(input("DIGITE UM NÚMERO: "))
+
+print("MAIOR: ", maior)
+print("MENOR: ", menor)
+print("FIM DO CÓDIGO")
